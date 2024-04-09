b03ae691025079d5e6d66f161e9aac23a1610c0e
JulieGab <jullygabriella.sa@gmail.com>
2024-04-09 20:53:58 -0300

    feat: operações aritiméticas

---
+++ 3-operacoesAritimeticas.js
@@ -0,0 +1,10 @@
+console.log('Operações aritiméticas');
+
+console.log(2+2)
+console.log(10+8*2)
+// assim como na matemática, temos que usar o parênteses para começar com a soma.
+console.log((10+8)*2)
+console.log('ano'+2020)
+// nesse caso, o JS fará a junção da string com o número, ficando "ano2020"
+console.log('2'+'2')
+// novamente será feita a junção, pois as aspas define o número como uma string, e não um numeral de fato
