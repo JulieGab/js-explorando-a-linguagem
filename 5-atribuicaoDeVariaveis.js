2c0d99f6b406eb12e794e3fc6750d919d21ba7a1
JulieGab <jullygabriella.sa@gmail.com>
2024-04-10 12:30:36 -0300

    fix: modificando nomes das vari

--- 5-atribuicaoDeVariaveis.js
+++ 5-atribuicaoDeVariaveis.js
@@ -1,18 +1,24 @@
 console.log('Trabalhando com atribuição de variáveis');
 
 const idade = 28;
-let nome = 'Gabi';
+let primeiroNome = 'Gabi';
 const sobrenome = 'Sá';
 
-console.log(nome, sobrenome);
+console.log(primeiroNome, sobrenome);
 // a vírgula também serve como separador
 // retorno: Gabi Sá
 // mesmo que: console.log(nome + ' ' + sobrenome)
 
-console.log(`meu nome é ${nome} ${sobrenome}`);
+console.log(`meu nome é ${primeiroNome} ${sobrenome}`);
 // esse é o meio mais utilizado hoje, por meio da interpolação utilizando ${}.
 
 // podemos também sobrescrever variáveis, dando um novo valor pra ela:
-nome = nome + ' '+ sobrenome;
+nomeCompleto = primeiroNome + ' '+ sobrenome;
 // repare que só foi possível porque a variável foi declarada como let, com "const" não seria possíve, por se tratar de uma constante.
-console.log(nome)
+console.log(nomeCompleto);
+
+let minhaIdade; // declarando a variável
+minhaIdade = 28 // atribuindo valor
+// diferente do const, pode ser atribuído em uma linha diferente. 
+minhaIdade = minhaIdade+1
+console.log(minhaIdade)
