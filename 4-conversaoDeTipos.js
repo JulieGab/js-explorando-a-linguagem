fd8db5a8963d891ea16f61be54950df7a5219b64
JulieGab <jullygabriella.sa@gmail.com>
2024-04-09 21:00:29 -0300

    feat: convertendo tipos

---
+++ 4-conversaoDeTipos.js
@@ -0,0 +1,15 @@
+console.log('Conversão de tipos');
+
+console.log('ano'+2020);
+// saída: "ano2020"
+console.log('2'+'2');
+// saída: "22", por conta da concatenação (junção)
+// nesse caso, para o programa ler como um valor numérico, podemos usar o parseInt, para converter em um valor inteiro:
+console.log(parseInt('2')+parseInt('2'));
+// saída:4
+
+console.log('10'/'2')
+// nesse caso, o retorno será 5, pois a divisão só faz sentido com elementos numéricos, diferente da soma, que pode ser entendida também como um concatenação de strings.
+
+console.log('Gabi'/'2')
+// NaN
