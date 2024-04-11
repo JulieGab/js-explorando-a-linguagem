add221f7b877042cd653a79fae75bc3b2c33d5c1
JulieGab <jullygabriella.sa@gmail.com>
2024-04-10 21:48:12 -0300

    feat: encadeando operadores lógicos

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -6,21 +6,22 @@ const listaDeCidades = [
     `Rio de Janeiro`
 ]
 
-const idadeComprador = 15;
+const idadeComprador = 18;
 const estaAcompanhada = true; 
+const temPassagemComprada = true;
 
 console.log('Destinos possíveis:');
 console.log(listaDeCidades)
 
-if(idadeComprador >= 18){
-    console.log('Comprador maior de idade');   
-    listaDeCidades.splice(1,1)
-} else if(estaAcompanhada){
-        console.log('Comprador está acompanhado');  
-        listaDeCidades.splice(1,1) 
-} else{
-        console.log('Comprador menor de idade, não pode comprar.');
-}
+// if(idadeComprador >= 18){
+//     console.log('Comprador maior de idade');   
+//     listaDeCidades.splice(1,1)
+// } else if(estaAcompanhada){
+//         console.log('Comprador está acompanhado');  
+//         listaDeCidades.splice(1,1) 
+// } else{
+//         console.log('Comprador menor de idade, não pode comprar.');
+// }
 
 
 console.log(listaDeCidades)
@@ -32,6 +33,18 @@ console.log(listaDeCidades)
 // console.log(idadeComprador <= 18);
 // console.log(idadeComprador == 18); // atribuição
 
+if (idadeComprador >= 18 || estaAcompanhada == true){ // concatenar operadores lógicos
+    console.log('Boa viagem!');   
+    listaDeCidades.splice(1,1);
+} else {
+    console.log('Comprador menor de idade, não pode comprar.');
+}
 
+console.log("Embarque: \n");
+if(idadeComprador >= 18 && temPassagemComprada){
+    console.log('Boa viagem!')
+}else{
+    console.log('Menor de idade, não pode embarcar!');
+}
 
-
+console.log(listaDeCidades);
